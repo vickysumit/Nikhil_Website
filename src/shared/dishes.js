c4ce831ae3742af13718531b56ec5b87b86bfdce
sumit <sharmasumit0308@gmail.com>
2020-07-25 15:46:40 +0530

    patanai

--- src/shared/dishes.js
+++ src/shared/dishes.js
@@ -1,17 +1,17 @@
 export const items = [
     {
       src: '/assets/images/sp5.png',
-      altText: 'Slide 1',
+      altText: 'S1',
       caption: 'Slide 1'
     },
     {
         src: '/assets/images/sp1.png',
-         altText: 'Slide 2',
+         altText: 'S 2',
         caption: 'Slide 2'
     },
     {
         src: '/assets/images/sp2.png',
-        altText: 'Slide 3',
+        altText: 'Sl 3',
         caption: 'Slide 3'
     }
   ];
